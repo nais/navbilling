060c2b1db431058a90915d9e9977735737bcf152
vebjorre <vebjorn.rekkebo@nav.no>
2022-11-23 14:51:57 +0100

    Add trailing new line
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

--- bigquery.tf
+++ bigquery.tf
@@ -6,8 +6,8 @@ resource "google_bigquery_dataset" "navbilling" {
 }
 
 resource "google_bigquery_table" "cost_breakdown_aiven" {
-  dataset_id = google_bigquery_dataset.navbilling.dataset_id
-  table_id   = "cost_breakdown_aiven"
+  dataset_id  = google_bigquery_dataset.navbilling.dataset_id
+  table_id    = "cost_breakdown_aiven"
   description = "Billing from aiven formatted to match gcp billing"
 
   view {
@@ -17,8 +17,8 @@ resource "google_bigquery_table" "cost_breakdown_aiven" {
 }
 
 resource "google_bigquery_table" "cost_breakdown_excluding_nais" {
-  dataset_id = google_bigquery_dataset.navbilling.dataset_id
-  table_id   = "cost_breakdown_excluding_nais"
+  dataset_id  = google_bigquery_dataset.navbilling.dataset_id
+  table_id    = "cost_breakdown_excluding_nais"
   description = "Billing from gcp for all projects excluding nais clusters as there is a separate logic for cost allocation in kubernetes"
 
   view {
@@ -28,8 +28,8 @@ resource "google_bigquery_table" "cost_breakdown_excluding_nais" {
 }
 
 resource "google_bigquery_table" "cost_breakdown_nais" {
-  dataset_id = google_bigquery_dataset.navbilling.dataset_id
-  table_id   = "cost_breakdown_nais"
+  dataset_id  = google_bigquery_dataset.navbilling.dataset_id
+  table_id    = "cost_breakdown_nais"
   description = "Billing for nais clusters allocated to namespaces and applications"
 
   view {
@@ -39,8 +39,8 @@ resource "google_bigquery_table" "cost_breakdown_nais" {
 }
 
 resource "google_bigquery_table" "cost_breakdown_total" {
-  dataset_id = google_bigquery_dataset.navbilling.dataset_id
-  table_id   = "cost_breakdown_total"
+  dataset_id  = google_bigquery_dataset.navbilling.dataset_id
+  table_id    = "cost_breakdown_total"
   description = "Combines all gcp and aiven billing"
 
   view {
@@ -50,8 +50,8 @@ resource "google_bigquery_table" "cost_breakdown_total" {
 }
 
 resource "google_bigquery_table" "gcp_billing_export" {
-  dataset_id = google_bigquery_dataset.navbilling.dataset_id
-  table_id   = "gcp_billing_export"
+  dataset_id  = google_bigquery_dataset.navbilling.dataset_id
+  table_id    = "gcp_billing_export"
   description = "Combines gcp billing from different points in time"
 
   view {
@@ -61,8 +61,8 @@ resource "google_bigquery_table" "gcp_billing_export" {
 }
 
 resource "google_bigquery_table" "gcp_projects_derived" {
-  dataset_id = google_bigquery_dataset.navbilling.dataset_id
-  table_id   = "gcp_projects_derived"
+  dataset_id  = google_bigquery_dataset.navbilling.dataset_id
+  table_id    = "gcp_projects_derived"
   description = "Data cleansing for gcp projects table"
 
   view {
