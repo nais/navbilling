21ba5808d0b0e5e7b085a3b1288badd8b9ed10ae
vebjorre <vebjorn.rekkebo@nav.no>
2022-11-23 12:54:58 +0100

    Editing one view
    Co-authored-by: Gøran Berntsen <goran.berntsen@nav.no>

--- bigquery.tf
+++ bigquery.tf
@@ -14,3 +14,20 @@ resource "google_bigquery_table" "test_view" {
     use_legacy_sql = false
   }
 }
+
+resource "google_bigquery_dataset" "test_terraform" {
+  dataset_id    = "test_terraform"
+  friendly_name = "test_terraform"
+  description   = "Testing terraform"
+  location      = "europe-north1"
+}
+
+resource "google_bigquery_table" "test_view1" {
+  dataset_id = google_bigquery_dataset.test_terraform.dataset_id
+  table_id   = "test_view1"
+
+  view {
+    query          = file("views/test.sql")
+    use_legacy_sql = false
+  }
+}
