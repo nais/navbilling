324f2523f5fa19a6b5938d998a17fb390c5e7711
vebjorre <vebjorn.rekkebo@nav.no>
2024-02-29 15:37:31 +0100

    include nais+ci-nais in nav costs

--- nais-billing-views/nais_billing_regional/tenant_nav.sql
+++ nais-billing-views/nais_billing_regional/tenant_nav.sql
@@ -1,2 +1,2 @@
 SELECT * FROM `nais-io.nais_billing_regional.cost_breakdown_total_daily_update` 
-where tenant in ('nav', 'example', 'testing', 'nais-dev', 'dev-nais')
+where tenant in ('nav', 'example', 'testing', 'nais-dev', 'dev-nais', 'nais', 'ci-nais')
