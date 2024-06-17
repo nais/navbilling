35725abe9c6b1bec68249611446b7e47ac13ce21
vebjorre <vebjorn.rekkebo@nav.no>
2024-06-17 09:38:19 +0200

    small comment

--- nais-billing-views/nais_billing_regional/cost_breakdown_nais.sql
+++ nais-billing-views/nais_billing_regional/cost_breakdown_nais.sql
@@ -12,7 +12,7 @@ SELECT b.project_name
         end as env
         , b.k8s_cluster as cluster
         , b.k8s_namespace as namespace
-        , case --TODO: håndtere knada-teams
+        , case -- Slettede team lables som nais fordi nais_billing_regional.nais_teams ikke har historikk
             when b.k8s_namespace in (select team from `nais_billing_regional.nais_teams`) and not starts_with(b.k8s_namespace, 'nais') then b.k8s_namespace
             when b.project_name in ('knada-gcp', 'knada-dev') then b.k8s_namespace
             else b.team 
