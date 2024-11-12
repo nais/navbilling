ff95d1389a8acf7b54a6a6f950ee134a6e1cf2e6
vebjorre <vebjorn.rekkebo@nav.no>
2024-11-12 11:12:50 +0100

    adjust to changes in naisbilling

--- views/nais_billing_tenants/monthly_tenant_billing.sql
+++ views/nais_billing_tenants/monthly_tenant_billing.sql
@@ -1,1 +1,7 @@
-select * from `nais-io.tenant_billing.monthly_tenant_billing`;
+select
+    invoice_month,
+    billing_year,
+    organization,
+    total_exact
+from
+    `nais-io.tenant_billing.monthly_tenant_billing`;
